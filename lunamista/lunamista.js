1191f80b2acdb3351d118fd40c510f65949ce0c0
fousseini konate <fkonat@pointpark.edu>
2017-09-21 12:38:46 -0400

    first commit

---
+++ lunamista/lunamista.js
@@ -0,0 +1,42 @@
+var express = require('express'); 
+var app = express(); 
+
+// set up handlebars view engine 
+var handlebars = require('express-handlebars').create({ defaultLayout:'main'}); 
+app.engine('handlebars', handlebars.engine); 
+app.set('view engine', 'handlebars');
+
+
+app.set('port', process.env.PORT || 3000); 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) { 
+	res.render('home'); 
+}); 
+
+app.get('/login', function(req, res) { 
+	res.render('login'); 
+});
+
+app.get('/new-user', function(req, res) {
+        res.render('new-user');
+}); 
+
+// custom 404 page 
+app.use(function(req, res, next) { 
+	res.status(404); 
+	res.render('404'); 
+});
+
+// custom 500 page 
+app.use(function(err, req, res, next) { 
+	console.error(err.stack); 
+	res.status(500); 
+	res.render('500'); 
+});
+
+app.listen(app.get('port'), function() { 
+	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' ); 
+});
+
+
